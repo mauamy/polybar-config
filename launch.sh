c210188bc265c08ef8316c1984df8e01153edb05
Yannick Mau <ymdev@xmau.de>
2020-07-10 04:50:52 +0200

    Add device.vars to handle device specific parameters like network interface names

--- launch.sh
+++ launch.sh
@@ -3,6 +3,9 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# set MONITOR variable
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
